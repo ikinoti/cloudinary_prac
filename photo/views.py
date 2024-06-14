a2a600bc5329c642b59159286dc82d0756d4d281
ikinoti <gitongakinoti1@gmail.com>
2024-06-14 20:25:44 +0300

    rendering our data from model

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Library
 
 # Create your views here.
+
+def index(request):
+
+    # import all the photos and save it in the db
+    photo = Library.objects.all()
+    context = {'photo':photo}
+    return render(request, 'photo/index.html', context)
